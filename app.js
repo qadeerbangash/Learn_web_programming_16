9dfec6ebbd27550d1dfa42b987a9f6ae84cc19cd
Qadeer Hussain <qadeeerhussain494@gmail.com>
2021-04-04 12:27:32 +0500

    Just Practice

---
+++ app.js
@@ -0,0 +1,58 @@
+
+// function readmore(){
+//     var t='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur nesciunt, repellendus harum deserunt, autem quia repudiandae minima perspiciatis exercitationem vero fugit laudantium corporis nisi. Alias pariatur tempora cumque id porro.'
+//     var p=document.getElementById('para')
+//     //innerHTML use for read the matiral
+//     p.innerHTML=t
+// }
+
+
+// //js mai hum html use kar sakty hai
+// function readmore(){
+//     var t='<ol><li>hi</li><li>hello</li><li>bye</li></ol>'
+//     var p=document.getElementById('para')
+//     //innerHTML use for read the matiral
+//     p.innerHTML=t
+// }
+
+// //chap#52
+// function big(){
+//     var img=document.getElementById('car')
+//     img.className = 'hidden'
+// }
+
+// //Chap 53
+// function changImage(){
+//     var carImage=document.getElementById('image')
+//     carImage.src='car2.jpg'
+// }
+
+// function changImageBefor(){
+//     var carImage=document.getElementById('image')
+//     carImage.src='car1.jpg'
+// }
+
+// //chap 55
+// function changImage(){
+//         var carImage=document.getElementById('image')
+//         carImage.style.width='80%'
+//     }
+// function changImage(){
+//     var carImage=document.getElementById('para')
+//     carImage.style.color='red'
+//     carImage.style.fontSize='3em'
+
+// }
+
+// //chap 56
+var para=document.getElementsByTagName('p')
+para[1].style.color='green'
+para[3].style.color='blue'
+para[1].style.backgroundColor='red'
+para[4].style.color='white'
+para[4].style.backgroundColor='black'
+
+
+
+
+
